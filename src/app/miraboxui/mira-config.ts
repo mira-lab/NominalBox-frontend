42658fd78dae33c3e0305d899bb235f83061268c
Daniil <34211974+daniilmp@users.noreply.github.com>
2018-09-06 08:56:13 +0300

    Changed contract's abi; Changed contract's address;

--- src/app/miraboxui/mira-config.ts
+++ src/app/miraboxui/mira-config.ts
@@ -1,5 +1,5 @@
 export const miraConfig = {
-  licenseContractAddress: '0x7de96ecd66921d3ba255c6a65f10a87f4146242a',
+  licenseContractAddress: '0x952f188f8a336f403326cd3ec08e702464bc3777',
   bufferContractAddress: '',
   miraFactoryContractAddress: '',
   miraCoinFaucetURL: 'http://185.244.217.49/faucet/get',
