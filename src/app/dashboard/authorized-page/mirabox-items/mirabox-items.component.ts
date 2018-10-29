b93973592a38e3bae30c729fe931e84f327e9398
Daniil <34211974+daniilmp@users.noreply.github.com>
2018-10-29 20:07:49 +0300

    Change mirabox currency component name to mirabox items so it makes sense; Moved logic to separate function; Removed unused directive; Renamed variable.

---
+++ src/app/dashboard/authorized-page/mirabox-items/mirabox-items.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+import {MiraBox} from '../../../mirabox/mirabox';
+import {CURRENCIES} from '../../create-page/currency/currency-list';
+import {PubkeyToAddressService} from '../../../mirabox/pubkey-to-address.service';
+
+@Component({
+  selector: 'app-mirabox-items',
+  templateUrl: './mirabox-items.component.html',
+  styleUrls: ['./mirabox-items.component.css']
+})
+export class MiraboxItemsComponent implements OnInit {
+  @Input() miraBox: MiraBox;
+  @Input() privateKey;
+  miraBoxItemsUI: any;
+
+  constructor(private pubToAddressSvc: PubkeyToAddressService) {
+  }
+
+  ngOnInit() {
+    this.miraBoxItemsUI = this.makeMiraBoxItemsForUI();
+  }
+  makeMiraBoxItemsForUI(): any {
+    return this.miraBox.getMiraBoxItems().map((item) => {
+      return {
+        currencySymbol: item.currency,
+        address: this.pubToAddressSvc.publicKeyToAddress(item.currency, item.address),
+        currencyImage: CURRENCIES.find(currency => currency.symbol === item.currency).icon,
+        currencyName: CURRENCIES.find(currency => currency.symbol === item.currency).name
+      };
+    });
+  }
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,12 +5,11 @@ import {AuthorizedPageComponent} from './authorized-page/authorized-page.compone
 import {ChangePinComponent} from './authorized-page/change-pin/change-pin.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {CurrencyComponent} from './create-page/currency/currency.component';
-import {CurrencyImageDirective} from './authorized-page/mirabox-currency/currency-image.directive';
 import {DashboardComponent} from './dashboard.component';
 import {GetPrivateKeysComponent} from './authorized-page/get-private-keys/get-private-keys.component';
 import {LastActionsComponent} from './authorized-page/last-actions/last-actions.component';
 import {MiraboxModule} from '../mirabox/mirabox.module';
-import {MiraboxCurrencyComponent} from './authorized-page/mirabox-currency/mirabox-currency.component';
+import {MiraboxItemsComponent} from './authorized-page/mirabox-items/mirabox-items.component';
 import {NoAuthRedirectService} from './no-auth-redirect.service';
 import {SaveBoxComponent} from './create-page/save-box/save-box.component';
 import {SharedModule} from '../shared/shared.module';
@@ -32,9 +31,7 @@ import {SharedModule} from '../shared/shared.module';
   ],
   declarations: [
     AuthorizedPageComponent,
-    MiraboxCurrencyComponent,
-    MiraboxCurrencyComponent,
-    CurrencyImageDirective,
+    MiraboxItemsComponent,
     LastActionsComponent,
     ChangePinComponent,
     GetPrivateKeysComponent,
